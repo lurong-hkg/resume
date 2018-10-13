6245ea08fa7aef1107355a087792c5d2b6597dab
lurong <lurong.hkg@gmail.com>
2018-10-13 16:14:53 -0700

    exclude git ,idea in s3

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 TARGET="s3://www.luronghk.com"
 
-aws s3 sync ${DIR}/ ${TARGET}
+aws s3 sync ${DIR}/ ${TARGET} --exclude=.git/* --exclude=.idea/*
